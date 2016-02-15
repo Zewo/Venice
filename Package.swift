c74b3db401c4b1c0cae3f12740ce99fa4107b477
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-15 20:46:51 -0200

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Venice",
     dependencies: [
-        .Package(url: "https://github.com/ZewoFlux/CLibvenice.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2)
     ]
 )
