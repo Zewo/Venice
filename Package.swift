d07db368fc79d779a6bdd5d652b35cd04d188003
antonmes <antonmes@users.noreply.github.com>
2016-03-16 00:56:51 +0300

    Add CLibvenice

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Venice",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2)
-    ]
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 4),
+    ],
+    targets: [Target(name: "Venice", dependencies: ["CLibvenice"])]
 )
