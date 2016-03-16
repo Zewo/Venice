ed1cab115d344270559a72c08a3a0bd1e9a19b47
antonmes <antonmes@users.noreply.github.com>
2016-03-16 18:39:55 +0300

    Fix C dependency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,5 @@ let package = Package(
     name: "Venice",
     dependencies: [
         .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 4),
-    ],
-    targets: [Target(name: "Venice", dependencies: ["CLibvenice"])]
+    ]
 )
