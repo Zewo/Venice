379de3476fa7dacdbe4e22dc2a93e26d42312b04
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-25 21:57:28 -0300

    move to VeniceX

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Venice",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/VeniceX/CLibvenice.git", majorVersion: 0, minor: 4),
     ]
 )
