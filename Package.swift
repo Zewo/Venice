977fa8582ca5fa854958eb8fdaa7bedad52a0465
Dan Appel <dan.appel00@gmail.com>
2016-05-27 17:16:50 -0700

    Update c7 version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Venice",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/CLibvenice.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8),
     ]
 )
