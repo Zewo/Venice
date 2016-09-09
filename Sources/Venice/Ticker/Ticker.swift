bd7e9d37cc3628d15eae4e219e3a891a71150b44
Romain Bossart <romain.bossart@fastmail.com>
2016-09-10 00:36:06 +0200

    Ticker produces UInt64 instead of Void

--- Sources/Venice/Ticker/Ticker.swift
+++ Sources/Venice/Ticker/Ticker.swift
@@ -25,10 +25,10 @@
 import C7
 
 public final class Ticker {
-    private let internalChannel = Channel<Void>()
+    private let internalChannel = Channel<UInt64>()
     private var stopped: Bool = false
 
-    public var channel: ReceivingChannel<Void> {
+    public var channel: ReceivingChannel<UInt64> {
         return internalChannel.receivingChannel
     }
 
@@ -37,7 +37,7 @@ public final class Ticker {
             while true {
                 nap(for: period)
                 if self.stopped { break }
-                self.internalChannel.send(Void())
+                self.internalChannel.send(UInt64(now()))
             }
         }
     }
--- Tests/VeniceTests/TickerTests.swift
+++ Tests/VeniceTests/TickerTests.swift
@@ -11,6 +11,20 @@ class TickerTests : XCTestCase {
         ticker.stop()
         nap(for: 20.milliseconds)
     }
+
+	func testTickerResolution() {
+		let ticker = Ticker(period: 10.milliseconds)
+		co {
+			var last: UInt64 = 0
+			for time in ticker.channel {
+				XCTAssertTrue(time - last >= UInt64(0))
+				last = time
+			}
+		}
+		nap(for: 100.milliseconds)
+		ticker.stop()
+		nap(for: 20.milliseconds)
+	}
 }
 
 extension TickerTests {
