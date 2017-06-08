a784245697c12f561bd6bccd0bc572f0fe9cce73
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-08 01:57:11 -0300

    install pkgconfig on linux travis

--- Package.swift
+++ Package.swift
@@ -2,9 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Venice",
-
     dependencies: [
         .Package(url: "https://github.com/Zewo/CLibdill.git", majorVersion: 1, minor: 0)
     ]
-
 )
