d51cac1b825c21ff159f0da3ed65293292a4c838
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-08 08:49:54 -0300

    remove minor

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
+// swift-tools-version:3.1
+
 import PackageDescription
 
 let package = Package(
     name: "Venice",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CLibdill.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/Zewo/CLibdill.git", majorVersion: 1)
     ]
 )
