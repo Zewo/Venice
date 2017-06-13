7ad47f12b1e7b47233313e66aabdd68610480d7c
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-13 14:53:41 -0300

    add test target

--- Package.swift
+++ Package.swift
@@ -17,9 +17,7 @@ let package = Package(
         .package(url: "https://github.com/Zewo/CLibdill.git", .branch("swift-4"))
     ],
     targets: [
-        .target(
-            name: "Venice",
-            dependencies: []
-        ),
+        .target(name: "Venice"),
+        .testTarget(name: "VeniceTests", dependencies: ["Venice"]),
     ]
 )
