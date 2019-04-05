6e4084827e7376a131751711fd29e16558211f89
Satish Babariya <satish.babariya@gmail.com>
2019-04-05 11:37:49 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Venice", targets: ["Venice"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Zewo/CLibdill.git", .branch("master"))
+        .package(url: "https://github.com/satishbabariya/CLibdill.git", .branch("master"))
     ],
     targets: [
         .target(name: "Venice", dependencies: ["CLibdill"]),
